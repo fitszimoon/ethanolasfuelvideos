370608c46bdae1ae2cf875419e3ccb37ca2f00b2
fitszimoon <fitszimoon@gmail.com>
2024-10-24 13:55:28 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,18 @@
 function toggleSidebar() {
     var sidebar = document.getElementById('sidebar');
     var mainContent = document.getElementById('video'); 
+    var iframes = document.querySelectorAll('iframe');  // Use the correct variable name
     sidebar.classList.toggle('open');
     mainContent.classList.toggle('shifted'); 
     document.getElementById('h1').classList.toggle('shifted');
     document.getElementById('h2').classList.toggle('shifted');
+    document.body.classList.toggle('sidebar-open');
+    
+    iframes.forEach(function(iframe) {
+        if (sidebar.classList.contains('open')) {
+            iframe.classList.add('sidebar-open');
+        } else {
+            iframe.classList.remove('sidebar-open');
+        }
+    });
 }
