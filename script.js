310d664c6f60fcbec6c1428a431010b4ee9f5fe5
fitszimoon <fitszimoon@gmail.com>
2024-10-25 14:35:53 +0800

    mobile Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,31 @@
+
+toggle-btn.addEventListener("click", toggleSidebar)
+toggle-btn.addEventListener("touchstart", toggleSidebar)
+toggle-btn.addEventListener("touchend", handleTouchEnd)
+
 function toggleSidebar() {
-    var sidebar = document.getElementById('sidebar');
-    var mainContent = document.getElementById('video'); 
-    var iframes = document.querySelectorAll('iframe');  // Use the correct variable name
-    sidebar.classList.toggle('open');
-    mainContent.classList.toggle('shifted'); 
-    document.getElementById('h1').classList.toggle('shifted');
-    document.getElementById('h2').classList.toggle('shifted');
-    document.body.classList.toggle('sidebar-open');
-    
-    iframes.forEach(function(iframe) {
-        if (sidebar.classList.contains('open')) {
-            iframe.classList.add('sidebar-open');
-        } else {
-            iframe.classList.remove('sidebar-open');
-        }
-    });
+  // Prevent the event from triggering twice on touch devices
+  event.preventDefault()
+
+  var sidebar = document.getElementById("sidebar")
+  var mainContent = document.getElementById("video")
+  var iframes = document.querySelectorAll("iframe")
+  var h1 = document.getElementById("h1")
+  var h2 = document.getElementById("h2")
+
+  // Toggle classes for sidebar, main content, headers, and body
+  sidebar.classList.toggle("open")
+  mainContent.classList.toggle("shifted")
+  h1.classList.toggle("shifted")
+  h2.classList.toggle("shifted")
+  document.body.classList.toggle("sidebar-open")
+
+  // Update iframe styles based on the sidebar state
+  iframes.forEach(function (iframe) {
+    if (sidebar.classList.contains("open")) {
+      iframe.classList.add("sidebar-open")
+    } else {
+      iframe.classList.remove("sidebar-open")
+    }
+  })
 }
